937c79d1a10f0286e0d8b64a5814c7507bbf1c35
Robert Walker <redsword_99@hotmail.co.uk>
2016-12-07 18:40:08 +0000

    Only import when required

--- encodesrv/logs/bots/__init__.py
+++ encodesrv/logs/bots/__init__.py
@@ -1,15 +1,13 @@
-from encodesrv.logs.bots import irc_
-from . import slack
-
-
 _bots = []
 
 
 def get_bot(bot, *args, **kwargs):
     
     if bot == 'irc_':
+        from . import irc_
         return_bot = irc_.Encode_irc(*args, **kwargs)
     elif bot == 'slack':
+        from . import slack
         return_bot = slack.Encode_slack(*args, **kwargs)
         
     _bots.append(return_bot)
